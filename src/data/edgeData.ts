ea6e62d076049dc76424e3909652fd7a45437332
Kianya707 <109903668+Kianya707@users.noreply.github.com>
2025-06-27 12:04:55 +0800

    Added Nodes

--- src/data/edgeData.ts
+++ src/data/edgeData.ts
@@ -1,58 +1,37 @@
-interface Edge {
-  node: [string, string],
-  port: [string, string],
-}
-
-interface EdgeData {
-  data: Edge[]
-}
+import {Connection, Edge, EdgeData} from './structs'
 
 const edge_data: EdgeData = {
   data: [
-  {
-    "node": ["r1", "sw1"],
-    "port": ["p2", "p1"]
+  {// Converge
+    "node": ["ict", "r1"],
+    "port": ["p2", "p1"],
+    "type": Connection.None
+  }, 
+  {// Govnet
+    "node": ["govnet", "r1"],
+    "port": ["",""],
+    "type": Connection.None
   },
   {
-    "node": ["r1", "fw1"],
-    "port": ["p1", "p1"]
+    "node": ["govnet", "sw5"],
+    "port": ["",""],
+    "type": Connection.None
+  },
+  {// Router
+    "node": ["r1", "sw5"],
+    "port": ["",""],
+    "type": Connection.None
   },
   {
-    "node": ["r1", "r2"],
-    "port": ["p7", "p1"]
+    "node": ["fw1", "fw2"],
+    "port": ["",""],
+    "type": Connection.None
   },
   {
-    "node": ["sw1", "sw2"],
-    "port": ["p11", "p1"]
-  },
-  {
-    "node": ["sw1", "lb1"],
-    "port": ["p2", "p2"]
-  },
-  {
-    "node": ["sw2", "ap1"],
-    "port": ["p6", "p1"]
-  },
-  {
-    "node": ["sw2", "h2"],
-    "port": ["p9", "p1"]
-  },
-  {
-    "node": ["sw2", "ap2"],
-    "port": ["p10", "p1"]
-  },
-  {
-    "node": ["h1", "ips1"],
-    "port": ["p3", "p1"]
-  },
-  {
-    "node": ["fw1", "lb1"],
-    "port": ["p3", "p1"]
-  },
-  {
-    "node": ["lb1", "ips1"],
-    "port": ["p4", "p2"]
-  }
+    "node": ["fw-pri", "fw-sec"],
+    "port": ["",""],
+    "type": Connection.None
+  },  
   ]
 }
 

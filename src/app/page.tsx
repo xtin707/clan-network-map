1921ec60945bc65e649ad337e06bac070ab0ad99
KaiserAce <lester.ignacio1209@gmail.com>
2025-06-30 16:02:20 +0800

    Better overview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,15 @@
-import SVGViewer from '../components/GraphViewer';
-import graphviz from 'graphviz-wasm';
-import node_data from '../data/nodeData';
-import edge_data from '../data/edgeData';
-import {generateOverviewGraph} from '../graph/overviewGraph';
+import node_data from '@/data/nodeData';
+import edge_data from '@/data/edgeData';
+import NetworkDiagram from '@/components/D3Graph';
 
 export default async function HomePage() {
-  const myDotString = generateOverviewGraph(node_data.data, edge_data.data);
-
-  let svgString: string = '';
-  let error: string | null = null;
-
-  try {
-    await graphviz.loadWASM();
-    svgString = graphviz.layout(myDotString, 'svg', 'dot');
-  } catch (e: any) {
-    console.error("Error generating SVG on server:", e);
-    error = `Failed to generate graph SVG on server: ${e.message || 'Unknown error'}`;
-  }
 
   return (
     <main className="p-5 text-center bg-[#8A1538] min-h-screen">
       <h1 className="text-4xl font-bold mb-4 text-[#FFB81C]">UPB Network Diagram</h1>
       <p className="text-lg mb-8 text-white">Click the node to to see the detailed view of ports</p>
-      <div className="max-w-4xl mx-auto">
-        {/* Pass the SVG string and error to the new component */}
-        <SVGViewer svgString={svgString} error={error} />
+      <div className="max-w-4xl mx-auto bg-gray-500">
+        <NetworkDiagram nodeData={node_data} edgeData={edge_data} width={1000} height={700}/>
       </div>
     </main>
   );

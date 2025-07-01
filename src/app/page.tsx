385b8aa47a1eed95ea9d774ee26bd541848cbc97
ARD <aadoctolero@up.edu.ph>
2025-07-01 10:39:56 +0800

    Refactored the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,27 @@ import edge_data from '@/data/edgeData';
 import NetworkDiagram from '@/components/D3Graph';
 
 
+
+
 export default async function HomePage() {
 
   return (
-    <main className="p-5 text-center bg-[#C6CDE0] min-h-screen">
-      <h1 className="text-4xl font-bold mb-4 text-black">UPB Network Diagram</h1>
-      <p className="text-lg mb-8 text-gray-500">Click the node to to see the detailed view of ports</p>
-      <div className="h-auto w-full bg-white justify-center border rounded-lg">
+    <main className="bg-white min-h-screen">
+      <header className="bg-black text-white flex items-center justify-between px-6 py-3">
+        <div className="flex items-center space-x-4">
+          <img src="/up-logo.png" alt="UPB Logo" className="h-10 w-auto"/>
+          <h1 className="text-xl font-semibold tracking-widest">UPB Network Diagram</h1>
+        </div>
+        
+        <div className="relative group">
+          <img src="/info.svg" alt="Info" className="w-6 h-6 cursor-pointer"/>
+          <div className="absolute right-0 mt-2 w-max bg-gray-800 text-white text-xs px-3 py-1 rounded shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            Click the node to see the detailed view of ports.
+          </div>
+        </div>
+      </header>
+
         <NetworkDiagram nodeData={node_data} edgeData={edge_data} width={1400} height={550}/>
-      </div>
     </main>
   );
 }

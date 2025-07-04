b21773ad809b51e689f899a53c8f819dc2d5c61b
KaiserAce <lester.ignacio1209@gmail.com>
2025-07-04 14:23:18 +0800

    nvmnd mas maganda toh

--- src/components/D3Graph.tsx
+++ src/components/D3Graph.tsx
@@ -18,7 +18,7 @@ const NetworkDiagram = ({ nodeData, edgeData, width, height }) => {
 
     // You can adjust the placement and size of nodes and edges here
     const simulation = d3.forceSimulation(nodes)
-      .force("link", d3.forceLink(links).id(d => d.id).distance(30).strength(1))
+      .force("link", d3.forceLink(links).id(d => d.id).distance(30).strength(0.8))
       .force("charge", d3.forceManyBody().strength(-600))
       .force("center", d3.forceCenter(width / 2, height / 2))
     .force("collide", d3.forceCollide(150).radius(40));

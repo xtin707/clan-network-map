ddb365d1226f1fc986d8d30e356e04aa6ab7ab0c
KaiserAce <lester.ignacio1209@gmail.com>
2025-07-16 08:31:16 +0800

    Node logging

--- src/data/nodeData.ts
+++ src/data/nodeData.ts
@@ -2335,6 +2335,253 @@ const node_data: NodeData = {
     ]
   },
 
+    {
+        "id": "l-tech24",
+        "label": "LIB-TECH24",
+        "ip": "10.200.106.6",
+        "type": Device.DistributionSwitch,
+        "ports": [
+            {
+            "id": "p1",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p2",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p3",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p4",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p5",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p6",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p7",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p8",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p9",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p10",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p11",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p12",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p13",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p14",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p15",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p16",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p17",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p18",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p19",
+            "label": "Voice VLAN 60 Ports",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p20",
+            "label": "Voice VLAN 60 Ports",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p21",
+            "label": "Voice VLAN 60 Ports",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p22",
+            "label": "Voice VLAN 60 Ports",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p23",
+            "label": "PublicIP Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p24",
+            "label": "Management Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p25",
+            "label": "LIB VLAN 106 Port",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p26",
+            "label": "Uplink Trunk to HPE-1810-8G",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p27",
+            "label": "Port 27",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p28",
+            "label": "Port 28",
+            "type": Connection.Ethernet,
+            },
+        ]
+    },
+
+    {
+        "id": "l-filipiniana",
+        "label": "LIB-FILIPINIANA",
+        "ip": "10.200.106.2",
+        "type": Device.DistributionSwitch,
+        "ports": [
+            {
+            "id": "p1",
+            "label": "Port 1",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p2",
+            "label": "Port 2",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p3",
+            "label": "J9802A-LIB-2F-TechRoom Bridge Bridge",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p4",
+            "label": "Port 4",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p5",
+            "label": "Port 5",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p6",
+            "label": "UPB-LibraryArchive-AP46 Bridge",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p7",
+            "label": "Port 7",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p8",
+            "label": "J9802A Bridge	Bridge",
+            "type": Connection.Ethernet,
+            },
+        ]
+    },
+
+    {
+        "id": "l-archives",
+        "label": "LIB-ARCHIVES",
+        "ip": "10.200.106.4",
+        "type": Device.DistributionSwitch,
+        "ports": [
+            {
+            "id": "p1",
+            "label": "Port 1",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p2",
+            "label": "DESKTOP-5406D6L 9C-6B-00-00-69-59",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p3",
+            "label": "Port 3",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p4",
+            "label": "Port 4",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p5",
+            "label": "Port 5",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p6",
+            "label": "Port 6",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p7",
+            "label": "J9802A Bridge	Bridge",
+            "type": Connection.Ethernet,
+            },
+            {
+            "id": "p8",
+            "label": "Port 8",
+            "type": Connection.Ethernet,
+            },
+        ]
+    },
+
   {
     "id": "rotc",
     "label": "ROTC",
